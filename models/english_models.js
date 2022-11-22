50d24823fd2fcae57cf67593f20d164633c7d0df
L-McHugh <l.mchugh1@outlook.com>
2022-11-22 11:57:28 +0000

    models and routes completed

---
+++ models/english_models.js
@@ -0,0 +1,71 @@
+import { query } from "../db/index.js";
+
+async function getEnglishDefinitions() {
+  try {
+    const allEnglishObject = await query("SELECT * FROM englishDefinitions");
+    return allEnglishObject.rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+async function getEnglishDefinitionByTitle(title) {
+  try {
+    const allEnglishObject = await query(
+      "SELECT * FROM englishDefinitions WHERE title = $1",
+      [title]
+    );
+    return allEnglishObject.rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+async function updateEnglishDefinition(id, title, definition, example, links, week){
+    try {
+        const updateEnglishObject = await query(
+        "UPDATE englishDefinitions SET title = $2, definition = $3, example = $4, links = $5, week = $6 WHERE id = $1 RETURNING *;",
+        [id, title, definition, example, links, week]
+        );
+        return updateEnglishObject.rows;
+    } catch (error) {
+        throw error;
+    }
+    }
+
+
+async function createEnglishDefinition(title, definition, example, links, week) {
+    try {
+        const createEnglishObject = await query(
+        "INSERT INTO englishDefinitions (title, definition, example, links, week) VALUES ($1, $2, $3, $4, $5) RETURNING *;",
+        [title, definition, example, links, week]
+        );
+        return createEnglishObject.rows;
+    } catch (error) {   
+        throw error;
+    }
+    }
+
+
+async function deleteEnglishDefinition(id) {
+  try {
+    const deleteEnglishObject = await query(
+      "DELETE FROM englishDefinitions WHERE id = $1 RETURNING *;",
+      [id]
+    );
+    return deleteEnglishObject.rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+export {
+    getEnglishDefinitions,
+    getEnglishDefinitionByTitle,
+    updateEnglishDefinition,
+    createEnglishDefinition,
+    deleteEnglishDefinition
+}
---
+++ routes/english_routes.js
@@ -0,0 +1,65 @@
+import { Express } from "express";
+const router = express.Router();
+
+import {
+    getEnglishDefinitions,
+    getEnglishDefinitionByTitle,
+    updateEnglishDefinition,
+    createEnglishDefinition,
+    deleteEnglishDefinition
+} from "../models/english_models";
+
+//const error.message = "Please try again later.";
+
+router.get("/", async (req, res) => {
+    try {
+        const allEnglishObject = await getEnglishDefinitions();
+        return res.json({success:true, payload: allEnglishObject});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+
+router.get("/:title", async (req, res) => {
+    try {
+        const englishObject = await getEnglishDefinitionByTitle(req.body.title);
+        return res.json({success:true, payload: englishObject});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+
+router.post("/", async (req, res) => {
+    try {
+        const createEnglishObject = await createEnglishDefinition(req.body.title, req.body.definition, req.body.example, req.body.links, req.body.week);
+        return res.json({success:true, payload: createEnglishObject});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+
+router.patch("/:id", async (req, res) => {
+    try {
+        const updateEnglishObject = await updateEnglishDefinition(req.params.id, req.body.title, req.body.definition, req.body.example, req.body.links, req.body.week);
+        return res.json({success:true, payload: updateEnglishObject});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const deleteEnglishObject = await deleteEnglishDefinition(req.params.id);
+        return res.json({success:true, payload: deleteEnglishObject});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+
+export default router;
+
