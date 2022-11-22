87e1d4cb886eed65b7e30ebad41c6f8172733a66
L-McHugh <l.mchugh1@outlook.com>
2022-11-22 12:18:40 +0000

    Paths all set up

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import morgan from "morgan";
+
+import { englishRouter } from "./routes/english_routes.js";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(morgan("dev"));
+app.use(express.static("public"));
+app.use(express.json());
+
+app.use("/api/englishDefinitions", englishRouter);
+
+app.listen(PORT, function () {
+    console.log(`Server listening on port ${PORT}`);
+  });
