aa89de5310539b32947ec8f06ab0f920dfe7c9e3
L-E-W-1-5 <lewiswootton88@hotmail.com>
2022-11-23 10:39:57 +0000

    cors

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ import germanRouter from "./routes/german_routes.js";
 import frenchRouter from "./routes/french_routes.js";
 import express from "express";
 import morgan from "morgan";
+
+import cors from 'cors'
 const PORT = process.env.PORT;
 
 
 
 const app = express();
 
-
+app.use(cors())
 app.use(morgan("dev"));
 app.use(express.json());
 //app.use(express.static("public"));
